ddd7c357461e2583699c579096f0282889ead1fa
niltonquinones-sy <nilton.quinones@gmail.com>
2025-10-20 20:34:04 -0500

    bug-faltaban los import

--- cliente.py
+++ cliente.py
@@ -1,8 +1,9 @@
 class Cliente:
-    def __init__(self, dni, nombre, direccion):
+    def __init__(self, dni, nombre, direccion, email):
         self.dni = dni
         self.nombre = nombre
         self.direccion = direccion
+        self.email = email
 
     def mostrar_info(self):
-        return f"[Cliente] {self.nombre} (DNI: {self.dni}), Dirección: {self.direccion}"
+        return f"[Cliente] {self.nombre} (DNI: {self.dni}), Email: {self.email}, Dirección: {self.direccion}"
